64d459249454364b948f5e80f985cf2d261cd3a0
carlosparaciari <carlo.sparaciari.14@ucl.ac.uk>
2021-06-10 11:45:24 +0100

    added a nice plot to the project

--- hello-world.py
+++ hello-world.py
@@ -1,4 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 print("hello planet")
 
 for i in range(4):
     print(i)
+
+x = np.arange(0,2*np.pi,0.1)
+y = np.sin(x)
+
+plt.plot(x,y)
+plt.title('test')
+plt.show()
