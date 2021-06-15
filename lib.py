47feee010c0853b17a82074f4c73efd4b906eecb
carlosparaciari <carlo.sparaciari.14@ucl.ac.uk>
2021-06-15 11:34:09 +0100

    typing added to functions

--- lib.py
+++ lib.py
@@ -1,8 +1,8 @@
-def increase(x):
+def increase(x : int) -> int:
     """ Increase the input by 1 """
     return x + 1
 
 
-def decrease(x):
+def decrease(x : int) -> int:
     """ Decrease the input by 1 """
     return x - 1
